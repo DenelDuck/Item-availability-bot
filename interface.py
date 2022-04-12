1409f669cdbe5291191db5f37280b72cd4835dca
Daniel <43882337+DenelDuck@users.noreply.github.com>
2022-04-12 14:24:25 +0200

    right click menu added and un-used imports removed

--- interface.py
+++ interface.py
@@ -1,21 +1,23 @@
-from concurrent.futures import thread
 import PySimpleGUI as sg
 import main
-import threading
 import sys
 import subprocess
 import main
 import pathlib
 import re
+import webbrowser
 
+#initial setup
 sg.theme('DarkAmber')
 with open(main.DEFAULT_FILE, "r") as file:
     data = file.read()
     lines = data.split("\n")
-layout = [  [sg.Text('Links to check:', size=(15, 1)), sg.InputText(key='-INPUT-', size=(80, 1), do_not_clear=False)],
-            [sg.Listbox(values=lines, size=(100, 10), key='-OUTPUT-')],
+layout = [  
+            [sg.Text('Links to check:', size=(15, 1)), sg.InputText(key='-INPUT-', size=(80, 1), do_not_clear=False)],
+            [sg.Listbox(values=lines, size=(100, 10), key='-OUTPUT-', right_click_menu=['&Right', ['Open Link', 'Copy Link']])],
             [sg.Button('Add'), sg.Button('Remove'), sg.Button('Check'), sg.Button('Exit')],
-            [sg.Listbox(values='', size=(100, 10), visible=False, key="-CMDOUT-")]]
+            [sg.Listbox(values='', size=(100, 10), visible=False, key="-CMDOUT-")]
+        ]
 
 window = sg.Window('Item availability bot', layout)
 
@@ -23,6 +25,7 @@ while True:
     event, values = window.read()
     if event == sg.WINDOW_CLOSED or event == 'Exit':
         break
+
     # Add link to file when button is pressed
     if event == 'Add':
         # dont add if nothing is filled in
@@ -36,6 +39,7 @@ while True:
                 file.write(f"\n{values['-INPUT-']}")
                 lines += [values['-INPUT-']]
                 window['-OUTPUT-'].update(values=lines)
+
     # remove selected link from file and list
     if event == 'Remove':
         if values['-OUTPUT-'] == []:
@@ -49,17 +53,21 @@ while True:
                     else:
                         file.write(f"\n{line}")
             lines.remove(values['-OUTPUT-'][0])
+            # update listbox with lines
             window['-OUTPUT-'].update(values=lines)
+            
     # execute check from main.py
     if event == 'Check':
         p = subprocess.Popen(f"C:\Python310\python.exe {pathlib.Path().resolve()}\main.py", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = []
         for line in p.stdout:    
             line = line.decode(errors='replace' if (sys.version_info) < (3, 5) else 'backslashreplace').rstrip()
-            if "stock" not in line:
-                continue
             # remove color codes
             line = re.sub(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]' , '' , line)
             output += [line + "\n"]
         # update listbox with output
         window['-CMDOUT-'].update(values=output, visible=True)
+    
+    if event == 'Open Link':
+        # open link in default browser
+        webbrowser.open(values['-OUTPUT-'][0])
